287ddde6eae521c2f4a8a718559222159f3c04b7
Viktor Sævarsson <viktor.saevars@gmail.com>
2017-12-12 13:43:57 +0000

    created drop.sql

---
+++ sql/drop.sql
@@ -0,0 +1,19 @@
+/* Election */
+DROP TABLE Votes;
+DROP TABLE Candidate;
+DROP TABLE Party;
+DROP TABLE County;
+
+/* BRFSS */
+DROP TABLE Result;
+DROP TABLE Footnote;
+DROP TABLE DataValueType;
+DROP TABLE Question;
+DROP TABLE Response;
+DROP TABLE BreakOut;
+DROP TABLE BreakOutCategory;
+DROP TABLE Topic;
+DROP TABLE Class;
+
+/* Shared */
+DROP TABLE State;
