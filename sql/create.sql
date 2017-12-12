ffa981fdc5d4211685895a5346e3c062f55c6b2e
Viktor Sævarsson <viktor.saevars@gmail.com>
2017-12-12 13:43:45 +0000

    added BRFSS tables

--- sql/create.sql
+++ sql/create.sql
@@ -1,8 +1,14 @@
+/*
+ * Shared tables
+ */
 CREATE TABLE State (
     state VARCHAR(2) PRIMARY KEY,
     description VARCHAR(100)
 );
 
+/*
+ * Election Tables
+ */
 CREATE TABLE County (
     fips INTEGER PRIMARY KEY,
     county VARCHAR(100),
@@ -23,4 +29,63 @@ CREATE TABLE Votes (
     county INTEGER REFERENCES County(fips),
     votes INTEGER,
     PRIMARY KEY(candidate, county)
+);
+
+
+/*
+ * BRFSS Tables
+ */
+CREATE TABLE Class (
+    id VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(400)
+);
+
+CREATE TABLE Topic (
+    id VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(400),
+    classId VARCHAR(10) REFERENCES Class(id)
+);
+
+CREATE TABLE BreakOutCategory (
+    id VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(400)
+);
+
+CREATE TABLE BreakOut (
+    id VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(400),
+    breakOutCategoryId VARCHAR(10) REFERENCES BreakOutCategory(id)
+);
+
+CREATE TABLE Response (
+    id VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(400)
+);
+
+CREATE TABLE Question (
+    id VARCHAR(10) PRIMARY KEY,
+    description VARCHAR(400),
+    topicId VARCHAR(10) REFERENCES Topic(id)
+);
+
+CREATE TABLE DataValueType (
+    type VARCHAR(100) PRIMARY KEY,
+    unit VARCHAR(10)
+);
+
+CREATE TABLE Footnote (
+    id SERIAL PRIMARY KEY,
+    symbol VARCHAR(10),
+    description VARCHAR(400)
+);
+
+CREATE TABLE Result (
+    id SERIAL PRIMARY KEY,
+    breakOutId VARCHAR(10) REFERENCES BreakOut(id),
+    dataValueType VARCHAR(100) REFERENCES DataValueType(type),
+    footnoteId INTEGER REFERENCES Footnote(id),
+    sampleSize INTEGER,
+    dataValue DECIMAL(1),
+    confidenceLimitLow DECIMAL(1),
+    confidenceLimitHigh DECIMAL(1)
 );
