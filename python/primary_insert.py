0950732508373d5c1b31f98e8b7a27d5d88df4f4
Viktor Sævarsson <viktor.saevars@gmail.com>
2017-12-14 14:27:51 +0000

    primary_insert.py now follows pep8

--- python/primary_insert.py
+++ python/primary_insert.py
@@ -13,22 +13,23 @@ except IndexError:
 
 cursor, conn = helper.connect_to_database(host, database, username, password)
 
+
 def fix_fips(row):
-	row['fips'] = row['fips'].split('.')[0]
-	if row['state'] == 'New Hampshire':
-		return {
-			'Belknap' : '001',
-			'Carroll' : '003',
-			'Cheshire' : '005',
-			'Coos' : '007',
-			'Grafton' : '009',
-			'Hillsborough' :'011',
-			'Merrimack' : '013',
-			'Rockingham': '015',
-			'Strafford' : '017',
-			'Sullivan' : '019',
-		}.get(row['county'], 'notfound')
-	return row['fips']
+    row['fips'] = row['fips'].split('.')[0]
+    if row['state'] == 'New Hampshire':
+        return {
+            'Belknap': '001',
+            'Carroll': '003',
+            'Cheshire': '005',
+            'Coos': '007',
+            'Grafton': '009',
+            'Hillsborough': '011',
+            'Merrimack': '013',
+            'Rockingham': '015',
+            'Strafford': '017',
+            'Sullivan': '019',
+        }.get(row['county'], 'notfound')
+    return row['fips']
 
 
 def state_insert(row):
@@ -37,12 +38,14 @@ def state_insert(row):
                    "ON CONFLICT DO NOTHING;", (row['state_abbreviation'],
                                                row['state']))
 
+
 def county_insert(row):
-	fips = fix_fips(row)
-	cursor.execute("INSERT INTO Counties "
-				   "VALUES (%s, %s, %s) "
-				   "ON CONFLICT DO NOTHING;\n", (fips, row['county'],
-				   							   row['state_abbreviation']))
+    fips = fix_fips(row)
+    cursor.execute("INSERT INTO Counties "
+                   "VALUES (%s, %s, %s) "
+                   "ON CONFLICT DO NOTHING;\n", (fips, row['county'],
+                                                 row['state_abbreviation']))
+
 
 def parties_insert(row):
     cursor.execute("INSERT INTO Parties "
@@ -53,7 +56,8 @@ def parties_insert(row):
 def condidates_insert(row):
     cursor.execute("INSERT INTO Candidates "
                    "VALUES (%s, %s)"
-                   "ON CONFLICT DO NOTHING;\n", (row['candidate'], row['party']))
+                   "ON CONFLICT DO NOTHING;\n", (row['candidate'],
+                                                 row['party']))
 
 
 def votes_insert(row):
@@ -61,25 +65,23 @@ def votes_insert(row):
     cursor.execute("INSERT INTO Votes "
                    "VALUES (%s, %s, %s)"
                    "ON CONFLICT DO NOTHING;\n", (row['candidate'],
-                                                 fips, 
-                                                 row['votes']))
+                                                 fips, row['votes']))
 
 
 with open(filename) as csvfile:
-	reader = csv.DictReader(csvfile)
+    reader = csv.DictReader(csvfile)
 
-	inserts = []
-	for idx, row in enumerate(reader):
-		
-		state_insert(row)
-		county_insert(row)
-		parties_insert(row)
-		condidates_insert(row)
-		votes_insert(row)
+    inserts = []
+    for idx, row in enumerate(reader):
+        state_insert(row)
+        county_insert(row)
+        parties_insert(row)
+        condidates_insert(row)
+        votes_insert(row)
 
-		if idx % 5000 == 0:
-			conn.commit()
-			print('{} values processed'.format(idx))
+        if idx % 5000 == 0:
+            conn.commit()
+            print('{} values processed'.format(idx))
 
 conn.commit()
 cursor.close()
